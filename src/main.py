2c311d24224e885485ae74dd8c1628ce309fa084
Hudson Newey <33742269+hudson-newey@users.noreply.github.com>
2022-11-18 20:01:51 +1000

    increased data points

--- src/main.py
+++ src/main.py
@@ -1,13 +1,26 @@
 from flask import Flask, request
 import requests
 import re
+import time
+from datetime import datetime
 
 from static import *
 
 app = Flask(__name__)
 
 def toJSON(coin = None, price = None, volume = None, change = None):
-  return "{ coin: \"" + str(coin) + "\", price: " + str(price) + ", volume: " + str(volume) + ", change: " + str(change) + " }"
+  return "{ coin: \"" + str(coin) + "\", date: \"" + getDate() + "\", timeHour: \"" + getTimeHour() + "\", timeMinute: \"" + getTimeMinute() + "\", price: " + str(price) + ", volume: " + str(volume) + ", change: " + str(change) + " }"
+
+def getDate():
+  return str(datetime.now())
+
+def getTimeMinute():
+  dateObj = datetime.now()
+  return str(dateObj.minute)
+
+def getTimeHour():
+  dateObj = datetime.now()
+  return str(dateObj.hour)
 
 @app.route('/')
 def root():
@@ -25,9 +38,9 @@ def root():
   htmlContent = exchange.text
   print(htmlContent)
 
-  exchangePrice = re.findall(PRICE_RE, htmlContent)[0].replace("Current price of Bitcoin is USD $", "")
-  exchangeVolume = re.findall(VOLUME_RE, htmlContent)[0].replace("24-hour trading volume of $", "")
-  exchangeChange = re.findall(CHANGE_RE, htmlContent)[0].replace("price is up ", "").replace("% in the last 24 hours.", "")
+  exchangePrice = re.findall(PRICE_RE, htmlContent)[0].replace("Current price of Bitcoin is USD $", "").replace(",", "")
+  exchangeVolume = re.findall(VOLUME_RE, htmlContent)[0].replace("24-hour trading volume of $", "").replace(",", "")
+  exchangeChange = re.findall(CHANGE_RE, htmlContent)[0].replace("price is up ", "").replace("% in the last 24 hours.", "").replace(",", "")
 
   return toJSON(requestedCrypto, exchangePrice, exchangeVolume, exchangeChange)
 
